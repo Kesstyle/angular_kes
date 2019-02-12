a82be8b15b05b29c5df6cac5e0f93ed418b4adb8
Kes Style <kess@tut.by>
2019-02-12 11:38:20 +0300

    Change structure of items (from goods to events)

--- src/app/purchase/purchase.component.ts
+++ src/app/purchase/purchase.component.ts
@@ -8,16 +8,20 @@ import { Component, OnInit, Input, Output } from '@angular/core';
 export class PurchaseComponent implements OnInit {
 
     @Input() nameNew: string;
-    @Input() priceNew: string;
+    @Input() dateDayNew: string;
+    @Input() dateMonthNew: string;
+    @Input() dateYearNew: string;
+    @Input() dateHourNew: string;
+    @Input() dateMinuteNew: string;
 
   i = 5;
 
   list = {
     items: [
-        { id: 1, purchase: "Хлеб", done: false, price: 15.9 },
-        { id: 2, purchase: "Масло", done: false, price: 60 },
-        { id: 3, purchase: "Картофель", done: true, price: 22.6 },
-        { id: 4, purchase: "Сыр", done: false, price: 310 }
+        { id: 1, purchase: "Хлеб", done: false, dateExpire: new Date('Mon Feb 11 2019 20:25:12 GMT+0300') },
+        { id: 2, purchase: "Масло", done: false, dateExpire: new Date('Mon Feb 11 2019 20:23:12 GMT+0300') },
+        { id: 3, purchase: "Картофель", done: true, dateExpire: new Date('Mon Feb 11 2019 20:22:12 GMT+0300') },
+        { id: 4, purchase: 'Сыр', done: false, dateExpire: new Date('2018-05-05T00:00:00') }
     ]
 };
 
@@ -36,10 +40,18 @@ export class PurchaseComponent implements OnInit {
 //   }
 
   addItem() {
-        const price = parseFloat(this.priceNew); // преобразуем введенное значение к числу
-        if (this.nameNew !== '' && !isNaN(price)) {
-            this.list.items.push({id: this.i, purchase: this.nameNew, price: price, done: false });
+        const dateDay = parseInt(this.dateDayNew, 10);
+        const dateMonth = parseInt(this.dateMonthNew, 10);
+        const dateYear = parseInt(this.dateYearNew, 10);
+        const dateHour = parseInt(this.dateHourNew, 10);
+        const dateMinute = parseInt(this.dateMinuteNew, 10);
+        if (this.nameNew !== '' && !isNaN(dateDay) && !isNaN(dateMonth) && !isNaN(dateYear)) {
+            const finalDateStr = dateYear + '-' + this.getDateStr(dateMonth) + '-' + this.getDateStr(dateDay) +
+             'T' + this.getDateStr(dateHour) + ':' + this.getDateStr(dateMinute) + ':00';
+            const finalDate = new Date(finalDateStr);
+            this.list.items.push({id: this.i, purchase: this.nameNew, dateExpire: finalDate, done: false });
             this.i++;
+            this.clearAll();
         }
   }
 
@@ -58,5 +70,18 @@ export class PurchaseComponent implements OnInit {
 
   changeCheckboxState (id: number) {
     this.list.items.filter(item => item.id === id).forEach(item => item.done = !item.done);
+  }
+
+  private getDateStr(date: number) {
+    return date < 10 ? '0' + date : date;
+  }
+
+  private clearAll() {
+    this.dateDayNew = '';
+    this.dateYearNew = '';
+    this.dateMonthNew = '';
+    this.dateHourNew = '';
+    this.dateMinuteNew = '';
+    this.nameNew = '';
   }
 }
